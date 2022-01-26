89840109b54d7554a9eda6203148ee926f494742
HARBA0Ui <7arba0ui@gmail.com>
2022-01-26 21:41:57 +0100

    first-commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const btns = document.querySelectorAll('.btn')
+const value = document.querySelector('.value')
+
+let count = 0;
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        console.log(e.target)
+        const style = e.currentTarget.classList
+        console.log(style)
+        if(style.contains('increase')){
+            count++;
+        }
+        else if(style.contains('decrease')){
+            count--;
+        }else{
+            count = 0;
+        }
+        if(count<0){
+            value.style.color = 'red'
+        }
+        if(0<=count){
+            value.style.color = "green"
+        }
+        value.textContent = count;
+    })
+})
+
